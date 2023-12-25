51ad2bc8c54b37cdd668cc0c25a6c38fd68de131
Dmitrii Vlasov <vlas.dv@gmail.com>
2023-12-25 21:40:37 +0100

    Add function for adding chosen amount of random generated Books to myLibrary

--- static/app.js
+++ static/app.js
@@ -1,3 +1,5 @@
+const bookList = document.querySelector('#books');
+
 const myLibrary = [];
 
 function Book(title, author, pages) {
@@ -11,3 +13,15 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function addRandomBooks(numberOfBooks) {
+  for (let i = 0; i < numberOfBooks; i++) {
+    const book = new Book(`Nice book ${i+1}`, `Great author ${i+1}`, Math.floor(Math.random() * 490) + 10);
+    addBookToLibrary(book);
+  }
+  console.log('books added:');
+  for (let book of myLibrary) {
+    console.log(`${book.title} ${book.author} ${book.pages}`);    
+  }
+}
+
+addRandomBooks(10);
